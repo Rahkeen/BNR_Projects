24475014150be750acb3110c1ad00681c396c9be
Rikin Marfatia <rikinm10@gmail.com>
2016-05-07 01:03:29 -0400

    testing to see if gitignore works

---
+++ GeoQuiz/app/src/main/java/bnr/rikinmarfatia/geoquiz/QuizActivity.java
@@ -0,0 +1,38 @@
+package bnr.rikinmarfatia.geoquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class QuizActivity extends AppCompatActivity {
+
+    private Button trueButton, falseButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+        trueButton = (Button) findViewById(R.id.true_btn);
+        trueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(QuizActivity.this,
+                               R.string.incorrect_toast,
+                               Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        falseButton = (Button) findViewById(R.id.false_btn);
+        falseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(QuizActivity.this,
+                               R.string.correct_toast,
+                               Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
