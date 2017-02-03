f54d8890e20722cf3aee56b4106013f4dcd61019
Rikin Marfatia <rikin@pelotoncycle.com>
2017-02-02 23:56:54 -0500

    Chapter-3 : Lifecycle Logging, Landscape Layout

--- GeoQuiz/app/src/main/java/bnr/rikinmarfatia/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/bnr/rikinmarfatia/geoquiz/QuizActivity.java
@@ -2,27 +2,42 @@ package bnr.rikinmarfatia.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
+    private static final String TAG = "QuizActivity";
+
     private Button trueButton;
     private Button falseButton;
+    private Button nextButton;
+    private TextView questionTextView;
+    private int currentIndex = 0;
+
+    private Question[] questions = new Question[] {
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_memes, false),
+            new Question(R.string.question_oceans, true)
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "In onCreate(bundle)");
         setContentView(R.layout.activity_quiz);
+
+        questionTextView = (TextView) findViewById(R.id.question_txtview);
+        setQuestion();
 
         trueButton = (Button) findViewById(R.id.true_btn);
         trueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this,
-                               R.string.incorrect_toast,
-                               Toast.LENGTH_SHORT).show();
+                checkAnswer(true);
             }
         });
 
@@ -30,10 +45,65 @@ public class QuizActivity extends AppCompatActivity {
         falseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this,
-                               R.string.correct_toast,
-                               Toast.LENGTH_SHORT).show();
+                checkAnswer(false);
             }
         });
+
+        nextButton = (Button) findViewById(R.id.next_btn);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentIndex = (currentIndex + 1) % questions.length;
+                setQuestion();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "In onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "In onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "In onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "In onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "In onDestroy()");
+    }
+
+    private void setQuestion() {
+        int question = questions[currentIndex].getQuestionTextResId();
+        questionTextView.setText(question);
+    }
+
+    private void checkAnswer(boolean pressedTrue) {
+        boolean answerTrue = questions[currentIndex].isAnswerTrue();
+        int messageResId;
+
+        if (pressedTrue && answerTrue || !pressedTrue && !answerTrue) {
+            messageResId = R.string.correct_toast;
+        } else {
+            messageResId = R.string.incorrect_toast;
+        }
+
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
     }
 }
